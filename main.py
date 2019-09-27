7318a723d947becc784d1d3a3855bea43a2add19
season101 <sijan.malla101@gmail.com>
2019-09-27 01:36:45 -0500

    Sep 27th Night coding Ends Up here. Frontend is almost half done. Need to see the menu options of 2nd data entry table.

--- main.py
+++ main.py
@@ -10,5 +10,44 @@
 #                          ?????????????????????????????????????  #
 #                                                                 #
 ##################### Program Begins ##############################
+print()
+print(" "*2+"=-"*32)
+print("{0:^64s}".format("Welcome! please complete your ticket order below!"))
+print(" "*2+"=-"*32)
+print()
+print()
 
-print("Welcome to the program")
+#Display for Theme Park Options
+print("="*24+" {0:^16s} ".format("THEME PARK OPTIONS")+"="*24)
+
+#Menu Items
+print("{0:^68s}".format("+"+"-"*26+"+"))
+print(" "*20+"| {0:^24s} |".format("THEME PARK"))
+print("{0:^68s}".format("+---+"+"-"*22+"+"))
+print(" "*20+"|{0:^3d}".format(1)+"| {0:^20s} |".format("Universal Studios"))
+print("{0:^68s}".format("+---+"+"-"*22+"+"))
+print(" "*20+"|{0:^3d}".format(2)+"| {0:^20s} |".format("Magic Kingdom"))
+print("{0:^68s}".format("+---+"+"-"*22+"+"))
+print(" "*20+"|{0:^3d}".format(3)+"| {0:^20s} |".format("Epcot"))
+print("{0:^68s}".format("+---+"+"-"*22+"+"))
+print()
+print()
+choice1ForThemePark = int(input("Choose a theme park (1-3): "))
+print()
+
+#Display for Ticket Options
+print("="*24+" {0:^16s} ".format(" TICKET OPTIONS ")+"="*24)
+
+print()
+
+print("{0:^68s}".format("+"+"-"*26+"+"))
+print(" "*20+"| {0:^24s} |".format("UNIVERSAL STUDIOS"))
+print("{0:^69s}".format("+---+"+"-"*11+"+"+"-"*10+"+"))
+
+#Menu Items
+print(" "*20+"|{0:^3d}".format(1)+"| {0:^20s} |".format("Universal Studios"))
+print("{0:^68s}".format("+---+"+"-"*22+"+"))
+print(" "*20+"|{0:^3d}".format(2)+"| {0:^20s} |".format("Magic Kingdom"))
+print("{0:^68s}".format("+---+"+"-"*22+"+"))
+print(" "*20+"|{0:^3d}".format(3)+"| {0:^20s} |".format("Epcot"))
+print("{0:^68s}".format("+---+"+"-"*22+"+"))
